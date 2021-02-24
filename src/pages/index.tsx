d31c48ff493f6200579b5307ce17c4f61ac993ba
wdrik <w.drik@outlook.com>
2021-02-24 13:42:53 -0300

    feat: add countdown start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,15 @@ import { Profile } from "../components/Profile";
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
